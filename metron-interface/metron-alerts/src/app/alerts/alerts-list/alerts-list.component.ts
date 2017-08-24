4937e44029c573fdb60eccfe9acf6ff65344ff48
iraghumitra <raghumitra.ksv@gmail.com>
2017-08-24 15:47:36 +0530

    Moved alerts table to seperate component

--- metron-interface/metron-alerts/src/app/alerts/alerts-list/alerts-list.component.ts
+++ metron-interface/metron-alerts/src/app/alerts/alerts-list/alerts-list.component.ts
@@ -49,7 +49,7 @@ export class AlertsListComponent implements OnInit, OnDestroy {
   alertsColumns: ColumnMetadata[] = [];
   alertsColumnsToDisplay: ColumnMetadata[] = [];
   selectedAlerts: Alert[] = [];
-  alerts: any[] = [];
+  alerts: Alert[] = [];
   alertsSearchResponse: AlertsSearchResponse = new AlertsSearchResponse();
   colNumberTimerId: number;
   refreshInterval = RefreshInterval.ONE_MIN;
@@ -117,16 +117,6 @@ export class AlertsListComponent implements OnInit, OnDestroy {
     });
   }
 
-  formatValue(column: ColumnMetadata, returnValue: string) {
-    try {
-      if (column.name.endsWith(':ts') || column.name.endsWith('timestamp')) {
-        returnValue = new Date(parseInt(returnValue, 10)).toISOString().replace('T', ' ').slice(0, 19);
-      }
-    } catch (e) {}
-
-    return returnValue;
-  }
-
   getAlertColumnNames(resetPaginationForSearch: boolean) {
     Observable.forkJoin(
       this.configureTableService.getTableMetadata(),
@@ -154,29 +144,6 @@ export class AlertsListComponent implements OnInit, OnDestroy {
     return fieldNames;
   }
 
-  getValue(alert: Alert, column: ColumnMetadata, formatData: boolean) {
-    let returnValue = '';
-    try {
-      switch (column.name) {
-        case 'id':
-          returnValue = alert[column.name];
-          break;
-        case 'alert_status':
-          returnValue = 'NEW';
-          break;
-        default:
-          returnValue = alert.source[column.name];
-          break;
-      }
-    } catch (e) {}
-
-    if (formatData) {
-      returnValue = this.formatValue(column, returnValue);
-    }
-
-    return returnValue;
-  }
-
   ngOnDestroy() {
     this.tryStopPolling();
   }
@@ -199,8 +166,9 @@ export class AlertsListComponent implements OnInit, OnDestroy {
     return false;
   }
 
-  onAddFilter(field: string, value: string) {
-    this.queryBuilder.addOrUpdateFilter(field, value);
+  onAddFilter(map: {}) {
+    let keys = Object.keys(map);
+    keys.forEach(key => { this.queryBuilder.addOrUpdateFilter(key, map[key]); });
     this.search();
   }
 
@@ -209,8 +177,8 @@ export class AlertsListComponent implements OnInit, OnDestroy {
     this.search();
   }
 
-  onGroupsChange(groups: string[]) {
-    this.queryBuilder.setGroupby(groups)
+  onGroupsChange(groups) {
+      
   }
   
   onPageChange() {
@@ -292,13 +260,6 @@ export class AlertsListComponent implements OnInit, OnDestroy {
     this.tryStartPolling();
   }
 
-  selectAllRows($event) {
-    this.selectedAlerts = [];
-    if ($event.target.checked) {
-      this.selectedAlerts = this.alerts;
-    }
-  }
-
   search(resetPaginationParams = true, savedSearch?: SaveSearch) {
     this.selectedAlerts = [];
 
@@ -328,32 +289,14 @@ export class AlertsListComponent implements OnInit, OnDestroy {
     this.tryStartPolling();
   }
 
-  selectRow($event, alert: Alert) {
-    if ($event.target.checked) {
-      this.selectedAlerts.push(alert);
-    } else {
-      this.selectedAlerts.splice(this.selectedAlerts.indexOf(alert), 1);
-    }
-  }
-
   setData(results: AlertsSearchResponse) {
     this.alertsSearchResponse = results;
     this.alerts = results.results;
     this.pagingData.total = results.total;
   }
 
-  showConfigureTable() {
+  showDetails() {
     this.saveRefreshState();
-    this.router.navigateByUrl('/alerts-list(dialog:configure-table)');
-  }
-
-  showDetails($event, alert: Alert) {
-    if ($event.target.type !== 'checkbox' && $event.target.parentElement.firstChild.type !== 'checkbox' && $event.target.nodeName !== 'A') {
-      this.selectedAlerts = [];
-      this.selectedAlerts = [alert];
-      this.saveRefreshState();
-      this.router.navigateByUrl('/alerts-list(dialog:details/' + alert.source['source:type'] + '/' + alert.source.guid + ')');
-    }
   }
 
   saveRefreshState() {
--- metron-interface/metron-alerts/src/app/alerts/alerts-list/table-view/table-view.component.ts
+++ metron-interface/metron-alerts/src/app/alerts/alerts-list/table-view/table-view.component.ts
@@ -1,15 +1,141 @@
-import { Component, OnInit } from '@angular/core';
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component, OnInit, OnChanges, Input, Output, EventEmitter, SimpleChanges } from '@angular/core';
+import {Router} from '@angular/router';
+
+import {Pagination} from '../../../model/pagination';
+import {SortEvent} from '../../../shared/metron-table/metron-table.directive';
+import {ColumnMetadata} from '../../../model/column-metadata';
+import {Alert} from '../../../model/alert';
 
 @Component({
   selector: 'app-table-view',
   templateUrl: './table-view.component.html',
   styleUrls: ['./table-view.component.scss']
 })
-export class TableViewComponent implements OnInit {
+export class TableViewComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  @Input() alerts = [];
+  @Input() pagingData = new Pagination();
+  @Input() selectedAlerts: Alert[] = [];
+  @Input() alertsColumnsToDisplay: ColumnMetadata[] = [];
+
+  @Output() onResize = new EventEmitter<void>();
+  @Output() onSort = new EventEmitter<SortEvent>();
+  @Output() onPageChange = new EventEmitter<void>();
+  @Output() onAddFilter = new EventEmitter<{}>();
+  @Output() selectedAlertsChange = new EventEmitter< Alert[]>();
+  @Output() beforeShowDetails = new EventEmitter<void>();
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['selectedAlerts'] && changes['selectedAlerts'].currentValue) {
+      console.log(this.selectedAlerts);
+    }
+  }
+
+  getValue(alert: Alert, column: ColumnMetadata, formatData: boolean) {
+    let returnValue = '';
+    try {
+      switch (column.name) {
+        case 'id':
+          returnValue = alert[column.name];
+          break;
+        case 'alert_status':
+          returnValue = 'NEW';
+          break;
+        default:
+          returnValue = alert.source[column.name];
+          break;
+      }
+    } catch (e) {}
+
+    if (formatData) {
+      returnValue = this.formatValue(column, returnValue);
+    }
+
+    return returnValue;
+  }
+
+  formatValue(column: ColumnMetadata, returnValue: string) {
+    try {
+      if (column.name.endsWith(':ts') || column.name.endsWith('timestamp')) {
+        returnValue = new Date(parseInt(returnValue, 10)).toISOString().replace('T', ' ').slice(0, 19);
+      }
+    } catch (e) {}
+
+    return returnValue;
+  }
+
+  pageChange() {
+    this.onPageChange.emit();
+  }
+
+  selectRow($event, alert: Alert) {
+    if ($event.target.checked) {
+      this.selectedAlerts.push(alert);
+    } else {
+      this.selectedAlerts.splice(this.selectedAlerts.indexOf(alert), 1);
+    }
+
+    this.selectedAlertsChange.emit(this.selectedAlerts);
+  }
+  
+  sort(sortEvent: SortEvent) {
+    this.onSort.emit(sortEvent);
+  }
+
+  selectAllRows($event) {
+    this.selectedAlerts = [];
+    if ($event.target.checked) {
+      this.selectedAlerts = this.alerts;
+    }
+
+    this.selectedAlertsChange.emit(this.selectedAlerts);
+  }
+
+  resize() {
+    this.onResize.emit();
+  }
+
+  addFilter(field: string, value: string) {
+    let map = {};
+    map[field] = value;
+    this.onAddFilter.emit(map);
+  }
+
+  showDetails($event, alert: Alert) {
+    if ($event.target.type !== 'checkbox' && $event.target.parentElement.firstChild.type !== 'checkbox' && $event.target.nodeName !== 'A') {
+      this.selectedAlerts = [];
+      this.selectedAlerts = [alert];
+      this.beforeShowDetails.emit();
+      this.selectedAlertsChange.emit(this.selectedAlerts);
+      this.router.navigateByUrl('/alerts-list(dialog:details/' + alert.source['source:type'] + '/' + alert.source.guid + ')');
+    }
+  }
+
+  showConfigureTable() {
+    this.beforeShowDetails.emit();
+    this.router.navigateByUrl('/alerts-list(dialog:configure-table)');
+  }
 }
